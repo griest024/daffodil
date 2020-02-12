e173723ae5d020be88e0b90a4ab68729e7329a21
Peter Lauck <griest024@gmail.com>
2020-02-12 15:47:58 -0500

    test(auth): improve expectation in auth transformer

--- libs/auth/src/drivers/magento/transforms/auth-transformer.service.spec.ts
+++ libs/auth/src/drivers/magento/transforms/auth-transformer.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { DaffAuthToken } from '@daffodil/auth';
+import { DaffAuthTokenFactory } from '@daffodil/auth/testing';
+
 import { DaffMagentoAuthTransformerService } from './auth-transformer.service';
 import { DaffAuthTransformer } from '../../injection-tokens/auth-transformer.token';
 import { GenerateTokenResponse } from '../models/outputs/generate-token-response';
 
 describe('DaffMagentoAuthTransformerService', () => {
   let service: DaffMagentoAuthTransformerService;
+
+  const authTokenFactory = new DaffAuthTokenFactory();
+
   let mockResponse: GenerateTokenResponse;
+  let mockAuthToken: DaffAuthToken;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,9 +26,11 @@ describe('DaffMagentoAuthTransformerService', () => {
     })
 
     service = TestBed.get(DaffAuthTransformer);
+
+    mockAuthToken = authTokenFactory.create();
     mockResponse = {
       generateCustomerToken: {
-        token: 'sdaf87sadbnfnsd'
+        token: mockAuthToken.token
       }
     };
   });
@@ -34,7 +43,7 @@ describe('DaffMagentoAuthTransformerService', () => {
     it('should return a DaffAuthToken with the correct token field', () => {
       const result = service.transform(mockResponse);
 
-      expect(result.token).toEqual(mockResponse.generateCustomerToken.token);
+      expect(result).toEqual(mockAuthToken);
     })
   })
 });
