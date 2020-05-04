5b58690a965aac51084308c3293f0ba217c3cc78
Peter Lauck <griest024@gmail.com>
2020-05-04 14:57:06 -0400

    feat(order): add reducers

--- libs/order/src/order-state.module.ts
+++ libs/order/src/order-state.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
-@NgModule({})
+import { DAFF_ORDER_STORE_FEATURE_KEY, daffOrderReducers } from './reducers/public_api';
+
+@NgModule({
+  imports: [
+    StoreModule.forFeature(DAFF_ORDER_STORE_FEATURE_KEY, daffOrderReducers),
+  ]
+})
 export class DaffOrderStateModule { }
