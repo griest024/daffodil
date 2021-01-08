33f67a373a01daaf3ede75a1600a0797442709b7
Peter Lauck <griest024@gmail.com>
2021-01-08 13:30:26 -0500

    refactor(design-land): async -> waitForAsync

--- apps/design-land/src/app/modal/modal.component.spec.ts
+++ apps/design-land/src/app/modal/modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DesignLandModalComponent } from './modal.component';
 
@@ -6,7 +6,7 @@ describe('ModalComponent', () => {
   let component: DesignLandModalComponent;
   let fixture: ComponentFixture<DesignLandModalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DesignLandModalComponent ]
     })
