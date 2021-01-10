589e9e78af72a277edd3292593253ef55d0bb61f
Peter Lauck <griest024@gmail.com>
2021-01-09 21:47:02 -0500

    test(order): add driver versions to test descriptions

--- libs/order/driver/magento/2.4.0/src/order.service.spec.ts
+++ libs/order/driver/magento/2.4.0/src/order.service.spec.ts
@@ -51,7 +51,7 @@ import {
 
 import { DaffOrderMagentoService } from './order.service';
 
-describe('Driver | Magento | Order | OrderService', () => {
+describe('Order | Driver | Magento | 2.4.0 | OrderService', () => {
   let service: DaffOrderMagentoService;
   let controller: ApolloTestingController;
 
