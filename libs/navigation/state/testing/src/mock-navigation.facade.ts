60371b0346870982308739cde97e19162b251642
Peter Lauck <griest024@gmail.com>
2021-01-11 11:20:28 -0500

    feat(navigation): provide mock facade in root and with useExisting

--- libs/navigation/state/testing/src/mock-navigation.facade.ts
+++ libs/navigation/state/testing/src/mock-navigation.facade.ts
@@ -8,7 +8,7 @@ import { DaffNavigationFacadeInterface } from '@daffodil/navigation/state';
  * A mock of the DaffNavigationFacade used to remove any interaction with the ngrx store.
  * This mock should be imported into tests using the DaffNavigationTestingModule.
  */
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class MockDaffNavigationFacade<T extends DaffGenericNavigationTree<T>> implements DaffNavigationFacadeInterface<T> {
   loading$: BehaviorSubject<boolean> = new BehaviorSubject(false);
   tree$: BehaviorSubject<T> = new BehaviorSubject(null);
