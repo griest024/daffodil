e8734c7beba508f62947a1d8d9b05082a057872c
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-09-10 18:14:22 -0700

    feat(eslint): add linting rules for testing

--- tools/eslint/config/index.js
+++ tools/eslint/config/index.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     'modules-newlines',
     'jasmine',
+    'jest',
     "eslint-plugin-import",
     "eslint-plugin-jsdoc",
     "eslint-plugin-prefer-arrow"
@@ -236,6 +237,12 @@ module.exports = {
     "@typescript-eslint/type-annotation-spacing": "error",
 
     // custom
+    "jest/max-nested-describe": [
+      "warn",
+      {
+        "max": 4
+      },
+    ],
     '@typescript-eslint/no-unused-vars': 'off',
     'prefer-arrow/prefer-arrow-functions': [
       'error',
